940270a2aafc9f83ef28739adcef1f9806916746
shotaro-dev <231440159+shotaro-dev@users.noreply.github.com>
2025-12-25 20:01:27 +0900

    Feat: CSS, HTML draft complete

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # movie-AI-app
 AI driven movie search app.
+
+## 要件
+- scrimbaのデザインに従う。
+
+## 仕様
+- tailwindを使う
+- 最初にユーザーに３つの質問を入力させる。
+- チャンク化したmovies.txtをopenaiでチャンクをベクトル化してsupabaseのベクトルdatabaseに保存する。
+- 質問のインプットを改行でjoinしてまとめて、ベクトル化して、vector databaseに類似度で検索して質問に近いdataをとってきて、それを改行でjoinしてchatCompletionに質問と一緒にinputして解答を出す。
+- chatのmessageはpushして追加していく。
+
+
+## 設計
+- 最初はviteでフロントでapi-key使う。
+- 最終的にvercelでserverless化する。
--- config.js
+++ config.js
@@ -2,15 +2,15 @@ import OpenAI from 'openai';
 import { createClient } from "@supabase/supabase-js";
 
 /** OpenAI config */
-if (!process.env.OPENAI_API_KEY) throw new Error("OpenAI API key is missing or invalid.");
+if (!process.meta.process.env.VITE_OPENAI_API_KEY) throw new Error("OpenAI API key is missing or invalid.");
 export const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: meta.process.env.VITE_OPENAI_API_KEY,
   dangerouslyAllowBrowser: true
 });
 
 /** Supabase config */
-const privateKey = process.env.SUPABASE_API_KEY;
-if (!privateKey) throw new Error(`Expected env var SUPABASE_API_KEY`);
-const url = process.env.SUPABASE_URL;
-if (!url) throw new Error(`Expected env var SUPABASE_URL`);
+const privateKey = process.meta.process.env.VITE_SUPABASE_API_KEY;
+if (!privateKey) throw new Error(`Expected env var VITE_SUPABASE_API_KEY`);
+const url = process.meta.process.env.VITE_SUPABASE_URL;
+if (!url) throw new Error(`Expected env var VITE_SUPABASE_URL`);
 export const supabase = createClient(url, privateKey);
